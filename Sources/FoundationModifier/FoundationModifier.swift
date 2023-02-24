43d8b035a5fb6e845d6b607f48fd9e05f5814af7
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-02-24 17:02:53 +0900

    add `Foundation` modifier

--- Sources/FoundationModifier/FoundationModifier.swift
+++ Sources/FoundationModifier/FoundationModifier.swift
@@ -1,6 +1,1 @@
-public struct FoundationModifier {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
+public struct FoundationModifier {}
---
+++ scripts/overload.py
@@ -0,0 +1,58 @@
+import glob
+import os
+import argparse
+
+generated_dir_path = '../Sources/FoundationModifier/Generated'
+
+tab = '    '
+
+overwrite = False
+target = 'Foundation'
+
+
+def generate(file: str) -> None:
+    class_name = file.split('/')[-1]
+    class_name = class_name.split('.')[0]
+    class_name = class_name.split('+')[0]
+
+    output_path = f"{generated_dir_path}/{target}/value/{class_name}+value.generated.swift"
+
+    f = open(file, 'r', encoding='UTF-8')
+    text = f.read()
+
+    text = text.replace('@discardableResult', f'@discardableResult\n{tab}@_disfavoredOverload')
+    text = text.replace('-> Self', '-> T')
+    text = text.replace('return Modify.DynamicMemberWrap(self.value)', 'return self.value')
+
+    f.close()
+
+    if overwrite or not os.path.exists(output_path):
+        f = open(output_path, 'w')
+        f.write(text)
+        f.close()
+
+
+def parse_args() -> None:
+    global overwrite
+
+    parser = argparse.ArgumentParser(
+        description="generate overload modifier"
+    )
+
+    parser.add_argument("--overwrite",
+                        action='store_true',
+                        help="overwrite generated files",
+                        default=False)
+
+    args = parser.parse_args()
+
+    overwrite = args.overwrite
+
+
+if __name__ == '__main__':
+    parse_args()
+
+    files = glob.glob(f'../Sources/FoundationModifier/Generated/{target}/wrap/*.swift')
+
+    for file in files:
+        generate(file)
